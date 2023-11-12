06e436b02dc900def677c2e13a0ea1c9d07f3fba
MolodykhVlad <Mr.Prins@yandex.ru>
2023-11-12 18:05:51 +0300

    Task7

---
+++ Tyuiu.MolodykhVY.Sprint4.Task7.V2.Test/DataServiceTest.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using Tyuiu.MolodykhVY.Sprint4.Task7.V2.Lib;
+
+namespace Tyuiu.MolodykhVY.Sprint4.Task7.V2.Test
+{
+    [TestClass]
+    public class DataServiceTest
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            DataService ds = new DataService();
+
+            string value = "597643158942";
+
+            int res = ds.Calculate(3, 4, value);
+
+            int wait = 39;
+
+            Assert.AreEqual(wait, res);
+        }
+    }
+}
---
+++ Tyuiu.MolodykhVY.Sprint4.Task7.V2/Program.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Tyuiu.MolodykhVY.Sprint4.Task7.V2.Lib;
+
+
+namespace Tyuiu.MolodykhVY.Sprint4.Task7.V2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DataService ds = new DataService();
+
+
+            Console.Title = "Спринт #4 | Выполнил: Молодых В. Ю. | ИИПБ-23-3";
+            Console.WriteLine("****************************************************************************************");
+            Console.WriteLine("* Спринт #4                                                                            *");
+            Console.WriteLine("* Тема: Обработка структурных типов                                                    *");
+            Console.WriteLine("* Задание #7                                                                           *");
+            Console.WriteLine("* Вариант #2                                                                           *");
+            Console.WriteLine("* Выполнил: Молодых Владислав Юрьевич | ИИПБ-23-3                                      *");
+            Console.WriteLine("****************************************************************************************");
+            Console.WriteLine("* УСЛОВИЕ:                                                                             *");
+            Console.WriteLine("* Дана строка из одноразрядных цифр '597643158942'.                                    *");
+            Console.WriteLine("* Преобразуйте ее в матрицу 3 на 4 и подсчитайте сумму нечетных чисел.                 *");
+            Console.WriteLine("****************************************************************************************");
+            Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                                     *");
+            Console.WriteLine("****************************************************************************************");
+
+
+            int[,] mtrx = new int[4, 3];
+            string value = "597643158942";
+
+
+
+            for (int i = 0; i < 4; i++)
+            {
+                for (int j = 0; j < 3; j++)
+                {
+                    mtrx[i, j] = int.Parse(value.Substring(i * 3 + j, 1));
+                }
+            }
+
+            Console.WriteLine("Строка: " + value);
+
+
+
+            Console.WriteLine();
+
+            Console.WriteLine("****************************************************************************************");
+            Console.WriteLine("* РЕЗУЛЬТАТ:                                                                           *");
+            Console.WriteLine("****************************************************************************************");
+
+            Console.WriteLine("Матрица 3 на 4:");
+
+            for (int i = 0; i < 4; i++)
+            {
+                for (int j = 0; j < 3; j++)
+                {
+                    Console.Write(mtrx[i, j] + " ");
+                }
+                Console.WriteLine();
+            }
+
+            Console.WriteLine("сумма нечетных чисел = " + ds.Calculate(3, 4, value));
+
+            Console.ReadLine();
+        }
+    }
+}
